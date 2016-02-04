5c38a39f9c1b028428798b963e335d401e853a9d
EMallmann <ericmallmann@gmail.com>
2016-02-03 19:11:48 -0600

    Create Shooter.cpp
    Shooter object

---
+++ src/Shooter.cpp
@@ -0,0 +1,26 @@
+/*
+ * Shooter.cpp 
+ *
+ *  Created on: Feb 3, 2016
+ *      Authors: Eric and Jack
+ */
+
+#include <Shooter.h>
+
+
+
+Shooter::Shooter() {
+	//instantiate necessary objects
+}
+
+void shoot(){
+	//use pneumatics or motor controllers
+}
+
+void takeInBall(){
+	//roll in ball w/ motor controllers
+}
+
+void angleBall(){
+	//lift ball w/ apparatus using button
+}
