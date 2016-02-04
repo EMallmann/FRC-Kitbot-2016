bb685bf1860aafb7c48f5a0b38151580c8434886
EMallmann <ericmallmann@gmail.com>
2016-02-03 19:08:33 -0600

    Create Manipulator.cpp
    Manipulator object

---
+++ src/Manipulator.cpp
@@ -0,0 +1,20 @@
+/*
+ * Manipulator.cpp
+ *
+ *  Created on: Feb 3, 2016
+ *      Authors: Eric and Jack
+ */
+
+#include <Manipulator.h>
+
+
+Manipulator::Manipulator() {
+	mnVic = new Victor(4);
+
+}
+
+bool manipulate(){
+	//use manipulator
+	return false;
+}
+
