052d9bd1ca63678344ea171b59f2ac6387592e47
EMallmann <ericmallmann@gmail.com>
2016-02-04 20:19:22 -0600

    Update DriveTrain.cpp
    Encoders/distance

--- src/DriveTrain.cpp
+++ src/DriveTrain.cpp
@@ -18,16 +18,25 @@ DriveTrain::DriveTrain() {
 		vic4= new Victor(0);
 		myRobot= new RobotDrive(vic1,vic2,vic3,vic4);
 
-		stick = new Joystick(0);
+		leftEncoder = new Encoder (2,3, true, CounterBase:: k4X); //check ports
+		rightEncoder = new Encoder (0,1, true, CounterBase:: k4X);
+		leftEncoder->SetDistancePerPulse((-1*4*3.14159)/360); //check circumference/(pulses per revolution)
+		rightEncoder->SetDistancePerPulse((-1*4*3.15159)/360);
+
+		leftStick = new Joystick(0);
+		rightStick= new Joystick(1);
 }
 	void DriveTrain::AutonomousInit(){
 		leftEncoder->Reset();
 		rightEncoder->Reset();}
 
 
-	void DriveTrain::TeleopInit(){}
+	void DriveTrain::TeleopInit(){
+		leftEncoder->Reset();
+		rightEncoder->Reset();
+	}
 	double DriveTrain::getThrottle(double val){
-		 float throttle = stick->GetThrottle();
+		 float throttle = leftStick->GetThrottle();
 		 throttle++;
 		 throttle = throttle *((1-val)/2);
 
@@ -40,8 +49,11 @@ DriveTrain::DriveTrain() {
 
 		float throttle = getThrottle(.4);
 
+		myRobot->TankDrive(leftStick->GetY()*throttle, rightStick->GetY()*throttle, true);
 
-		myRobot->ArcadeDrive(stick->GetY()*throttle, stick->GetTwist(),true);
+		SmartDashboard::PutNumber("left distance", leftEncoder->GetDistance());
+		SmartDashboard::PutNumber("right distance", rightEncoder->GetDistance());
+		//myRobot->ArcadeDrive(stick->GetY()*throttle, stick->GetTwist(),true);
 	}
 
 
